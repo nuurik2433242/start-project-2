c9a2033b5764ca8d29d167033272fd00d1324bcd
nuurik2433242 <tasmaganbetnurblat@gmail.com>
2024-03-24 16:27:10 +0500

    added top slider section

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ async function styles() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
