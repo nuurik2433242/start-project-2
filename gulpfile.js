ecdb044d2b1bd4b23dbc705c1dada4e4523081e2
nuurik2433242 <tasmaganbetnurblat@gmail.com>
2024-03-31 18:03:40 +0500

    added video-fashion

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,8 @@ function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
-        'app/js/main.js'
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'app/js/main.js' // Исправленный путь к основному скрипту
     ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
@@ -68,3 +69,4 @@ exports.build = build;
 
 // Задача по умолчанию
 exports.default = parallel(styles, scripts, browsersync, watching);
+
