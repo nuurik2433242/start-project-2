097806bf05e6c30e89b276141ac7387a53207e37
nuurik2433242 <tasmaganbetnurblat@gmail.com>
2024-05-16 18:24:28 +0500

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
 $(function(){
 
+  $('.product-tabs__top-item').on('click', function(e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  
+});
+
+
+
+
+
+  $('.product-slide__thumb').slick({
+    asNavFor :'.product-slide__big',
+    focusOnSelect : true,
+    slidesToShow : 4,
+    slidesToScroll : 1,
+    vertical : true,
+    draggable : false
+  });
+
+  $('.product-slide__big').slick({
+    asNavFor :'.product-slide__thumb',
+    draggable : false,
+    arrows : false,
+    fade : true
+  });
+
   $(function(){
     $('.shop-content__filter-btn').on('click', function () {
         $('.shop-content__filter-btn').css('color', '#8d8d8d');
@@ -21,7 +51,7 @@ $(function(){
   });
 
 
-  $('.select-style').styler();
+  $('.select-style, .product-one__item-num').styler();
 
 
 
